2d47b457b633268bb8b4be762c987154465eb136
Hyq0719 <2418011730@qq.com>
2022-02-05 00:02:33 +0800

    init marketing-api-generator-vivo

---
+++ code-generators/vivo-code-generator/src/main/java/com/hyq0719/mktapi/generator/vivo/entity/GeneratorConfigEntity.java
@@ -0,0 +1,29 @@
+package com.hyq0719.mktapi.generator.vivo.entity;
+
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * @program: dashboard
+ * @description:
+ * @author: wuning
+ * @create: 2020-09-29 10:42
+ **/
+@Data
+public class GeneratorConfigEntity {
+  private String mainPackage;
+  private Person person;
+  private Map<String, JavaMap> generatorJavaMap;
+
+  @Data
+  public static class Person {
+    String author;
+    String email;
+  }
+
+  @Data
+  public static class JavaMap {
+    String packageName;
+  }
+}
