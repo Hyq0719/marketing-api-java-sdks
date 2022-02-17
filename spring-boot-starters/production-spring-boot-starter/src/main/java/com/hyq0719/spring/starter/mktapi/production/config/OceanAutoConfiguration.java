8399b31406a87ca7fa486ab20279bb5e5036d763
Hyq0719 <2418011730@qq.com>
2022-02-17 10:04:00 +0800

    init production-spring-boot-starter

---
+++ spring-boot-starters/production-spring-boot-starter/src/main/java/com/hyq0719/spring/starter/mktapi/production/config/OceanAutoConfiguration.java
@@ -0,0 +1,91 @@
+package com.hyq0719.spring.starter.mktapi.production.config;
+
+import com.hyq0719.mktapi.common.executor.http.OkhttpHttpHandler;
+import com.hyq0719.mktapi.common.token.ITokenCronService;
+import com.hyq0719.mktapi.common.token.cache.ITokenLocalCache;
+import com.hyq0719.mktapi.oceanengine.OceanApiClient;
+import com.hyq0719.mktapi.oceanengine.OceanRetryStrategy;
+import com.hyq0719.mktapi.oceanengine.service.OceanSdkService;
+import com.hyq0719.mktapi.oceanengine.token.OceanExternalTokenService;
+import com.hyq0719.spring.starter.mktapi.production.properties.SdkProperties;
+import com.hyq0719.spring.starter.mktapi.production.properties.SdkProperties.ChannelConfig;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@EnableConfigurationProperties(SdkProperties.class)
+@Import(HttpAutoConfiguration.class)
+@Data
+@Slf4j
+public class OceanAutoConfiguration implements CommandLineRunner {
+
+  private ITokenCronService oceanTrigger;
+
+  @Bean
+  @ConditionalOnMissingBean
+  public OceanSdkService oceanSdkService(OceanApiClient oceanApiClient, OceanRetryStrategy oceanRetryStrategy) {
+    log.info("SDK bulid oceanSdkService, oceanApiClient:{},oceanRetryStrategy:{}", oceanApiClient, oceanRetryStrategy);
+    return new OceanSdkService(oceanApiClient, oceanRetryStrategy);
+  }
+
+  @Bean
+  public OceanRetryStrategy oceanRetryStrategy(SdkProperties sdkProperties) {
+    OceanRetryStrategy oceanRetryStrategy = new OceanRetryStrategy();
+    ChannelConfig oceanengine = sdkProperties.getOceanengine();
+    if (oceanengine == null) {
+      throw new RuntimeException("oceanengine properties is null");
+    }
+    oceanRetryStrategy.setRetryCount(oceanengine.getRetryCount());
+    oceanRetryStrategy.setEnable(oceanengine.getEnableRetry());
+    return oceanRetryStrategy;
+  }
+
+  @Bean
+  public ITokenLocalCache oceanCache() {
+    return new ITokenLocalCache();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public OceanApiClient oceanApiClient(OkhttpHttpHandler httpsClient, OceanExternalTokenService oceanExternalTokenService) {
+    if (httpsClient == null) {
+      throw new RuntimeException("oceanengine RequestHandler is null");
+    }
+    if (oceanExternalTokenService == null) {
+      throw new RuntimeException("OceanExternalTokenService is null");
+    }
+    return new OceanApiClient(oceanCache(), httpsClient, oceanExternalTokenService);
+  }
+
+  @Bean
+  public ITokenCronService oceanCronService(OceanExternalTokenService oceanExternalTokenService, SdkProperties sdkProperties) {
+    String cron = sdkProperties.getOceanengine().getCron();
+    if (StringUtils.isEmpty(cron)) {
+      throw new RuntimeException("oceanengine cron is null");
+    }
+    ITokenCronService simpleCronService = new ITokenCronService(oceanExternalTokenService, oceanCache(),
+            sdkProperties.getOceanengine().getCron());
+    simpleCronService.run();
+    oceanTrigger = simpleCronService;
+    return simpleCronService;
+  }
+
+  @Override
+  public void run(String... args) {
+    log.info("oceanengine Trigger obj:{}", oceanTrigger);
+    try {
+      oceanTrigger.trigger();
+    } catch (Exception e) {
+      log.info("load 数据 oceanengine failure");
+      e.printStackTrace();
+      log.info("oceanengine sdk start run error:{}", e.getMessage());
+    }
+  }
+}
